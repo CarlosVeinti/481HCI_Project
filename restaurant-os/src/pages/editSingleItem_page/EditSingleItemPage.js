18d234c0bf5514c391b739da3593ec89150dcbcf
CarlosVeinti <carlosveintimillam@hotmail.com>
2023-10-30 17:17:03 -0600

    Customer Pages Templates
    All customer page templates now uploaded.

---
+++ restaurant-os/src/pages/editSingleItem_page/EditSingleItemPage.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import WelcomeSection from '../../components/WelcomeSection/WelcomeSection';
+import LanguageButton from '../../components/LanguageButton/LanguageButton';
+import CategoriesButtons from '../../components/CategoriesButtons/CategoriesButtons';
+
+function EditSingleItemPage() {
+    return (
+        <>
+        <div className="landing-page">
+            <div className='cover-title'>
+                <h1>Sushi-Ya</h1>
+            </div>
+            
+        </div>
+        </>
+    );
+}
+
+export default EditSingleItemPage;
