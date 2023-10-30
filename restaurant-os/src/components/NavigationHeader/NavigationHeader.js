1a7f372971239cc557df11bc6e5036549d9f4b37
CarlosVeinti <carlosveintimillam@hotmail.com>
2023-10-30 17:56:18 -0600

    Added Navigation Bar to Categories Page

---
+++ restaurant-os/src/components/NavigationHeader/NavigationHeader.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import Button from 'react-bootstrap/Button';
+import { Navbar } from 'react-bootstrap';
+
+const circleButtonStyle = {
+    backgroundColor: '#719579', // Replace with your desired color
+    color: 'black',
+    border: '2px solid #8D6E63',
+    width: '75px',  // Adjust the width and height as needed for your design
+    height: '75px',
+    borderRadius: '50%',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    margin: '0 40px',
+  };
+
+const brandStyle = {
+    border: '2px solid #8D6E63', // Border around the text
+    borderRadius: '10%', // Make the border round
+    padding: '5px', // Padding to separate the border from text
+};
+
+function NavigationHeader() {
+    return (
+        <div className='navHeader'>
+            <Navbar>
+                <Button variant="primary" style={circleButtonStyle}>Back</Button>
+                <Button variant="primary" style={circleButtonStyle}>Request Bill</Button>
+                <Navbar.Brand>
+                    <span style={brandStyle}>Sushi-Ya</span>
+                </Navbar.Brand>
+                <Button variant="primary" style={circleButtonStyle}>Back</Button>
+                <Button variant="primary" style={circleButtonStyle}>Request Bill</Button>
+            </Navbar>
+        </div>
+    );
+}
+
+export default NavigationHeader;
--- restaurant-os/src/pages/categories_page/CategoriesPage.js
+++ restaurant-os/src/pages/categories_page/CategoriesPage.js
@@ -3,14 +3,14 @@ import React from 'react';
 import WelcomeSection from '../../components/WelcomeSection/WelcomeSection';
 import LanguageButton from '../../components/LanguageButton/LanguageButton';
 import CategoriesButtons from '../../components/CategoriesButtons/CategoriesButtons';
+import NavigationHeader from '../../components/NavigationHeader/NavigationHeader'
 
 function CategoriesPage() {
     return (
         <>
         <div className="landing-page">
-            <div className='cover-title'>
-                <h1>Sushi-Ya</h1>
-            </div>
+            
+            <NavigationHeader/>
             <CategoriesButtons />
         </div>
         </>
