78cd581aa7d55e48e98bc6347b738efbf8f99d01
Evan Wong <evanwongkh@hotmail.com>
2023-10-31 19:08:27 -0600

    Started CartPage

--- restaurant-os/src/index.js
+++ restaurant-os/src/index.js
@@ -37,6 +37,10 @@ const router = createBrowserRouter([
     element: <EditSingleItemPage />,
 
   },
+  {
+    path: "/cart",
+    element: <CartPage />,
+  }
 ]);
 
 
--- restaurant-os/src/pages/cart_page/CartPage.js
+++ restaurant-os/src/pages/cart_page/CartPage.js
@@ -1,19 +1,42 @@
 import React from 'react';
 import WelcomeSection from '../../components/WelcomeSection/WelcomeSection';
 import LanguageButton from '../../components/LanguageButton/LanguageButton';
+import './CartPage.css';
 
 function CartPage() {
     return (
-        <>
-        <div>
-            <img src="/logo.png" alt="Restaurant Logo" className='cover-image'></img>
-        </div>
+        <div className="cart-container">
+          <h2>Your Cart</h2>
+          <div className="cart-items">
+            <div className="cart-item">
+            <span>Uptown Ramen</span>
+            <div className="quantity-container">
+            <button className="btn btn-outline-secondary">-</button>
+            <span>Quantity: 1</span>
+            <button className="btn btn-outline-secondary">+</button>
+            </div>
+            <span>$16.00 each</span>
+            <span>Total: $16.00</span>
+            </div>
 
-        <div className="landing-page">
-            
+            <div className="cart-item">
+            <span>California Roll</span>
+            <div className="quantity-container">
+            <button className="btn btn-outline-secondary">-</button>
+            <span>Quantity: 2</span>
+            <button className="btn btn-outline-secondary">+</button>
+            </div>
+            <span>$10.00 each</span>
+            <span>Total: $20.00</span>
+            </div>
+            {/* Your cart items will go here */}
+          </div>
+          <div className="cart-summary">
+            <p>Total: $36.00</p>
+            <button className="btn btn-success">Proceed to Checkout</button>
+          </div>
         </div>
-        </>
-    );
+      );
 }
 
 export default CartPage;
