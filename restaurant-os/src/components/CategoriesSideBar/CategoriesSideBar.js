f12799cd1cc9d8032307e0101691144c2190f2da
CarlosVeinti <carlosveintimillam@hotmail.com>
2023-12-01 00:00:31 -0700

    Side Bar Categories Buttons now function

--- restaurant-os/src/components/CategoriesSideBar/CategoriesSideBar.js
+++ restaurant-os/src/components/CategoriesSideBar/CategoriesSideBar.js
@@ -1,43 +1,61 @@
 import React from 'react';
-import CategoryCard from '../../components/CategoryCard/CategoryCard';
-import CategoriesButtons from '../../components/CategoriesButtons/CategoriesButtons';
-import NavigationHeader from '../../components/NavigationHeader/NavigationHeader';
 import './CategoriesSideBar.css';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Button from 'react-bootstrap/Button';
+import { useNavigate } from 'react-router-dom';
 
 const customBackgroundStyle = {
     backgroundColor: '#fff5ee', // Replace with your desired background color
 };
 
 function CategoriesSideBar() {
+
+    const navigate = useNavigate();
+
+    const handleCategoryClick = (category) => {
+        // Use the category to construct the URL for the corresponding BrowseItemsPage
+        const categoryPath = `/browse-items/${category.toLowerCase()}`;
+        navigate(categoryPath);
+    };
     return (
       <>        
           <Container className='sidebarCategs'>
             <Row className="catSideRows">
-                <Button variant="primary" className='catSide_Btn'>Appetizers</Button>
+                <Button variant="primary" className='catSide_Btn' onClick={() => handleCategoryClick('appetizers')}>
+                    Appetizers
+                </Button>
             </Row>
 
             <Row className="catSideRows">
-                <Button variant="primary" className='catSide_Btn'>Sushi</Button>
+                <Button variant="primary" className='catSide_Btn' onClick={() => handleCategoryClick('sushi')}>
+                    Sushi
+                </Button>
             </Row>
 
             <Row className="catSideRows" >
-                <Button variant="primary" className='catSide_Btn'>Sashimi</Button>
+                <Button variant="primary" className='catSide_Btn' onClick={() => handleCategoryClick('sashimi')}>
+                    Sashimi
+                </Button>
             </Row>
 
             <Row className="catSideRows">
-                <Button variant="primary" className='catSide_Btn'>Ramen</Button>
+                <Button variant="primary" className='catSide_Btn' onClick={() => handleCategoryClick('ramen')}>
+                    Ramen
+                </Button>
             </Row>
 
             <Row className="catSideRows">
-                <Button variant="primary" className='catSide_Btn'>Desserts</Button>
+                <Button variant="primary" className='catSide_Btn' onClick={() => handleCategoryClick('desserts')}>
+                    Desserts
+                </Button>
             </Row>
 
             <Row className="catSideRows">
-                <Button variant="primary" className='catSide_Btn'>Beverages</Button>
+                <Button variant="primary" className='catSide_Btn' onClick={() => handleCategoryClick('beverages')}>
+                    Beverages
+                </Button>
             </Row>
 
            
