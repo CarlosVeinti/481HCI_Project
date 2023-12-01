64ca5199206c115cb8fa21bf8e9b3fe9d55d7a54
Evan Wong <evanwongkh@hotmail.com>
2023-11-30 18:43:53 -0700

    Added nav header to CartPage

--- restaurant-os/src/pages/cart_page/CartPage.js
+++ restaurant-os/src/pages/cart_page/CartPage.js
@@ -1,192 +1,94 @@
-import React from 'react';
-import WelcomeSection from '../../components/WelcomeSection/WelcomeSection';
-import LanguageButton from '../../components/LanguageButton/LanguageButton';
+import React, {useState} from 'react';
+import ItemCard from '../../components/ItemCard/ItemCard';
+import CartItem from '../../components/CartItem/CartItem';
 import './CartPage.css';
 import { FaTrash } from 'react-icons/fa'; // Import the trash can icon
+import NavigationHeader from '../../components/NavigationHeader/NavigationHeader';
+
 
 function CartPage() {
+    const [cartItems, setCartItems] = useState([
+        {
+            itemName: "Uptown Ramen",
+            description: "Remove pork",
+            quantity: 1,
+            price: 16.0,
+            imageSrc: "/uptownramen.jpg"
+        },
+        {
+            itemName: "California Roll",
+            description: "",
+            quantity: 1,
+            price: 10.0,
+            imageSrc: "/californiaroll.jpg"
+        },
+        {
+            itemName: "Dynamite Roll",
+            description: "",
+            quantity: 1,
+            price: 11.0,
+            imageSrc: "/dynamiteroll.jpg"
+        },
+        {
+            itemName: "Kappa Maki (6pcs)",
+            description: "",
+            quantity: 2,
+            price: 6.0,
+            imageSrc: "/kappamaki.jpeg"
+        },
+        {
+            itemName: "Matcha Cheesecake",
+            description: "",
+            quantity: 1,
+            price: 6.0,
+            imageSrc: "/desert.png"
+        },
+        {
+            itemName: "Philadelphia Roll",
+            description: "Extra avocado",
+            quantity: 1,
+            price: 12.0,
+            imageSrc: "/californiaroll.jpg"
+        },
+        {
+            itemName: "Tempura Udon",
+            description: "",
+            quantity: 1,
+            price: 14.0,
+            imageSrc: "/tempuraudon.jpg"
+        }
+    ]);
+
+    const calculateTotalPrice = () => {
+        return cartItems.reduce((total, item) => total + item.price * item.quantity, 0).toFixed(2);
+    }
+
+    const removeFromCart = (itemName) => {
+        setCartItems((prevItems) => prevItems.filter((item) => item.itemName !== itemName));
+    }
+
     return (
-        <div className="cart-container">
-          <h2>Your Cart</h2>
-          <div className="cart-items">
+        <div className="nav_Header">
+        <NavigationHeader />
+            <div className="cart-container">
+            <h2>Your Cart</h2>
+            <div className="cart-items">
+                {cartItems.map((item) => (
+                <CartItem key={item.itemName} item={item} removeFromCart={removeFromCart} />
+                ))}
+                </div>
+
+                <p className = "total-price">Total: ${calculateTotalPrice()}</p>
+                <div className = "returnsubmitbutton-container">
+                    <div className = "return-to-menu">
+                        <button className = "btn btn-primary">Return to Menu</button>
+                    </div>
+                    <div className="cart-summary">
+                        <button className="btn btn-success">Submit Order</button>
+                    </div>
+                </div>
+            </div>
             
-            <div className="cart-item">
-                <div className="item-details">
-                    <img src="/uptownramen.jpg" alt="Food Item" className="food-image" />
-                    <div className="item-info">
-                        <h3>Uptown Ramen</h3>
-                        
-                        <p>Remove pork</p>
-                    </div>
-                </div>
-                <div className="quantity-container">
-                    <button className="btn btn-outline-secondary mr-2">-</button>
-                    <span>1</span>
-                    <button className="btn btn-outline-secondary">+</button>
-                </div>
-            <span className="cart-item-price">$16.00</span>
-            <button className="btn btn-link remove-button">
-            <FaTrash />
-            </button>
-            </div>
-
-            <div className="cart-item">
-                <div className="item-details">
-                    <img src="/californiaroll.jpg" alt="Food Item" className="food-image" />
-                    <div className="item-info">
-                        <h3>California Roll</h3>
-                        
-                        <p></p>
-                    </div>
-                </div>
-                <div className="quantity-container">
-                    <button className="btn btn-outline-secondary mr-2">-</button>
-                    <span>1</span>
-                    <button className="btn btn-outline-secondary">+</button>
-                </div>
-            <span className="cart-item-price">$10.00</span>
-            <button className="btn btn-link remove-button">
-            <FaTrash />
-            </button>
-            </div>
-
-            <div className="cart-item">
-                <div className="item-details">
-                    <img src="/dynamiteroll.jpg" alt="Food Item" className="food-image" />
-                    <div className="item-info">
-                        <h3>Dynamite Roll</h3>
-                        
-                        <p></p>
-                    </div>
-                </div>
-                <div className="quantity-container">
-                    <button className="btn btn-outline-secondary mr-2">-</button>
-                    <span>1</span>
-                    <button className="btn btn-outline-secondary">+</button>
-                </div>
-            <span className="cart-item-price">$11.00</span>
-            <button className="btn btn-link remove-button">
-            <FaTrash />
-            </button>
-            </div>
-
-            <div className="cart-item">
-                <div className="item-details">
-                    <img src="/kappamaki.jpeg" alt="Food Item" className="food-image" />
-                    <div className="item-info">
-                        <h3>Kappa Maki (6pcs)</h3>
-                        
-                        <p></p>
-                    </div>
-                </div>
-                <div className="quantity-container">
-                    <button className="btn btn-outline-secondary mr-2">-</button>
-                    <span>2</span>
-                    <button className="btn btn-outline-secondary">+</button>
-                </div>
-            <span className="cart-item-price">$12.00</span>
-            <button className="btn btn-link remove-button">
-            <FaTrash />
-            </button>
-            </div>
-
-            <div className="cart-item">
-                <div className="item-details">
-                    <img src="/desert.png" alt="Food Item" className="food-image" />
-                    <div className="item-info">
-                        <h3>Matcha Cheesecake</h3>
-                        
-                        <p></p>
-                    </div>
-                </div>
-                <div className="quantity-container">
-                    <button className="btn btn-outline-secondary mr-2">-</button>
-                    <span>1</span>
-                    <button className="btn btn-outline-secondary">+</button>
-                </div>
-            <span className="cart-item-price">$6.00</span>
-            <button className="btn btn-link remove-button">
-            <FaTrash />
-            </button>
-            </div>
-
-            <div className="cart-item">
-                <div className="item-details">
-                    <img src="/philadelphiaroll.jpg" alt="Food Item" className="food-image" />
-                    <div className="item-info">
-                        <h3>Philadelphia Roll</h3>
-                        
-                        <p>Extra avocado</p>
-                    </div>
-                </div>
-                <div className="quantity-container">
-                    <button className="btn btn-outline-secondary mr-2">-</button>
-                    <span>1</span>
-                    <button className="btn btn-outline-secondary">+</button>
-                </div>
-            <span className="cart-item-price">$12.00</span>
-            <button className="btn btn-link remove-button">
-            <FaTrash />
-            </button>
-            </div>
-
-            <div className="cart-item">
-                <div className="item-details">
-                    <img src="/tempuraudon.jpg" alt="Food Item" className="food-image" />
-                    <div className="item-info">
-                        <h3>Tempura Udon</h3>
-                        
-                        <p></p>
-                    </div>
-                </div>
-                <div className="quantity-container">
-                    <button className="btn btn-outline-secondary mr-2">-</button>
-                    <span>1</span>
-                    <button className="btn btn-outline-secondary">+</button>
-                </div>
-            <span className="cart-item-price">$14.00</span>
-            <button className="btn btn-link remove-button">
-            <FaTrash />
-            </button>
-            </div>
-
-            <div className="cart-item">
-                <div className="item-details">
-                    <img src="/gyoza.jpg" alt="Food Item" className="food-image" />
-                    <div className="item-info">
-                        <h3>Gyoza (6 pcs)</h3>
-                        
-                        <p></p>
-                    </div>
-                </div>
-                <div className="quantity-container">
-                    <button className="btn btn-outline-secondary mr-2">-</button>
-                    <span>1</span>
-                    <button className="btn btn-outline-secondary">+</button>
-                </div>
-            <span className="cart-item-price">$6.00</span>
-            <button className="btn btn-link remove-button">
-            <FaTrash />
-            </button>
-            </div>
-
-
-          </div>
-          <p className="total-price">Total: $87.00</p>
-
-
-          <div className="button-container">
-            
-            <div className="return-to-menu">
-                <button className="btn btn-primary">Return to Menu</button>
-            </div>
-
-            <div className="cart-summary">
-                <button className="btn btn-success">Submit Order</button>
-            </div>
-
-          </div>
-
         </div>
       );
 }
