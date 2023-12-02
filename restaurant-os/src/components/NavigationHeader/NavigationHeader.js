5386c27bfed5c4e502b91b5d670d17ff1e20ae7f
JohnnyYuen1 <101444834+JohnnyYuen1@users.noreply.github.com>
2023-12-02 16:59:11 -0700

    Added request bill and waiter modal

--- restaurant-os/src/components/NavigationHeader/NavigationHeader.js
+++ restaurant-os/src/components/NavigationHeader/NavigationHeader.js
@@ -1,61 +1,82 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Navbar from 'react-bootstrap/Navbar';
 import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Image from 'react-bootstrap/Image';
 import './NavigationHeader.css'; // Import your custom styles
 import { useNavigate } from 'react-router-dom';
-import { FaShoppingCart } from 'react-icons/fa'; // Replace with the desired icon
-import { FaArrowLeft } from 'react-icons/fa'; // Replace with the desired icon
-import { FaReceipt } from "react-icons/fa";
-
+import { FaShoppingCart, FaArrowLeft, FaReceipt } from 'react-icons/fa';
 
 function NavigationHeader() {
+  const [showCallWaiterModal, setShowCallWaiterModal] = useState(false);
+  const [showRequestBillModal, setShowRequestBillModal] = useState(false);
+
+  const handleCallWaiterClick = () => setShowCallWaiterModal(true);
+  const handleRequestBillClick = () => setShowRequestBillModal(true);
+
+  const handleCloseModals = () => {
+    setShowCallWaiterModal(false);
+    setShowRequestBillModal(false);
+    // Reset splitBill state when closing modals
+  };
+
   const navigate = useNavigate();
+
   return (
     <>
       <Navbar className="customNavbar">
-
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
-
         <Navbar.Collapse id="basic-navbar-nav">
           <div className="navbarButtonsContainer">
-            {/* Your navigation links go here */}
-            <Button 
-              variant="primary" 
-              className="navbarButtons" 
-              onClick={() => navigate(-1)}>
-                <FaArrowLeft size={30}/>  Back
+            <Button variant="primary" className="navbarButtons" onClick={() => navigate(-1)}>
+              <FaArrowLeft size={30} /> Back
             </Button>
-            <Button 
-              variant="primary" 
-              className="navbarButtons">
-                <FaReceipt size={30}/> Request Bill
+            <Button variant="primary" className="navbarButtons" onClick={handleRequestBillClick}>
+              <FaReceipt size={30} /> Request Bill
             </Button>
-
             <Navbar.Brand className="customNavbarBrand">
-              <Image
-                src='/logo.png'
-                alt="Logo"
-                width="64"
-                height="64"
-                className="customImage"
-              />
+              <Image src="/logo.png" alt="Logo" width="64" height="64" className="customImage" />
               <span className="customMargin"></span>
             </Navbar.Brand>
-
-            {/* Your navigation links go here */}
-            <Button variant="primary" className="navbarButtons">Call Waiter</Button>
-            <Button 
-              variant="primary" 
-              className="navbarButtons"
-              onClick={() => navigate("/cart")} >
-                <FaShoppingCart size={30}/> Cart
+            <Button variant="primary" className="navbarButtons" onClick={handleCallWaiterClick}>
+              Call Waiter
+            </Button>
+            <Button variant="primary" className="navbarButtons" onClick={() => navigate("/cart")}>
+              <FaShoppingCart size={30} /> Cart
             </Button>
           </div>
         </Navbar.Collapse>
-
       </Navbar>
+
+      {/* Modal for Call Waiter */}
+      <Modal show={showCallWaiterModal} onHide={handleCloseModals}>
+        <Modal.Header closeButton>
+          <Modal.Title>Call Waiter</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <p style={{ color: 'black' }}>A waiter will be with you shortly.</p>
+        </Modal.Body>
+      </Modal>
+
+      {/* Modal for Request Bill */}
+      <Modal show={showRequestBillModal} onHide={handleCloseModals}>
+        <Modal.Header closeButton>
+          <Modal.Title>Request Bill</Modal.Title>
+        </Modal.Header>
+        <Modal.Body className="text-center">
+        <p style={{ color: 'black' }}>Would you want to split the bill?</p>
+          <div className="d-flex justify-content-center mb-3">
+            <Button variant="success" onClick={handleCallWaiterClick}>
+              Yes
+            </Button>
+            <div className="mx-2"></div>
+            <Button variant="danger" onClick={handleCallWaiterClick}>
+              No
+            </Button>
+          </div>
+        </Modal.Body>
+      </Modal>
     </>
   );
 }
