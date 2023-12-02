33a5968a963f4522c4f1acee85bccf010826bac2
TTan66 <113965025+TTan66@users.noreply.github.com>
2023-12-01 22:32:08 -0700

    SingleItemPage

--- restaurant-os/src/components/IngredientButtons/ServingButton.js
+++ restaurant-os/src/components/IngredientButtons/ServingButton.js
@@ -1,18 +1,23 @@
 import React, { useState } from 'react';
 import './ServingButton.css';
 
-function ServingCounter() {
-    const [servings, setServings] = useState(1); 
+function ServingCounter({ onServingChange }) {
+  const [servings, setServings] = useState(1); 
 
-    return (
-        <div className="serving-counter">
-            <button onClick={() => setServings(servings - 1)} disabled={servings <= 1}>-</button>
-            <div className="serving-display">
-                {servings} servings
-            </div>
-            <button onClick={() => setServings(servings + 1)}>+</button>
-        </div>
-    );
+  const updateServings = (newServings) => {
+    setServings(newServings);
+    onServingChange(newServings);
+  };
+
+  return (
+    <div className="serving-counter">
+      <button onClick={() => updateServings(servings - 1)} disabled={servings <= 1}>-</button>
+      <div className="serving-display">
+        {servings} servings
+      </div>
+      <button onClick={() => updateServings(servings + 1)}>+</button>
+    </div>
+  );
 }
 
 export default ServingCounter;
--- restaurant-os/src/pages/editSingleItem_page/EditSingleItemPage.js
+++ restaurant-os/src/pages/editSingleItem_page/EditSingleItemPage.js
@@ -1,87 +1,135 @@
 import React, { useState } from 'react';
-import IngredientButtonGroup from '../../components/IngredientButtons/ToggleButton'
-import ServingCounter from '../../components/IngredientButtons/ServingButton'
+import IngredientButtonGroup from '../../components/IngredientButtons/ToggleButton';
+import ServingCounter from '../../components/IngredientButtons/ServingButton';
 import './EditSingleItemPage.css';
 import NavigationHeader from '../../components/NavigationHeader/NavigationHeader';
 import BackButton from '../../components/BackButton/BackButton';
-import { Button } from 'react-bootstrap';
 import { useLocation } from 'react-router-dom';
 
 const customBackgroundStyle = {
-    backgroundColor: '#fff5ee', // Replace with your desired background color
+    backgroundColor: '#fff5ee',
 };
 
 function EditSingleItemPage() {
-
-
-    
-
-
-    /*const { itemName, description, imageSrc, price } = location.state*/
     const location = useLocation();
     const { itemName, imageSrc, price, longDescri } = location.state || {};
-    //console.log(itemName)
-    const  [editMode,setEditMode]= useState(true)
+    const [editMode, setEditMode] = useState(true);
+
+    const [ingredientSelections, setIngredientSelections] = useState({
+        ingredient1: 'regular',
+        ingredient2: 'regular',
+        ingredient3: 'regular',
+        ingredient4: 'regular',
+        ingredient5: 'regular'
+    });
+    
+    const [servings, setServings] = useState(1);
+
+    const prices = {
+        regular: 1.00,
+        remove: 0.00,
+        double: 2.00,
+        triple: 3.00,
+    };
+    
+    // Function to handle selection change
+    const handleSelectionChange = (ingredientId, selection) => {
+        setIngredientSelections(prev => {
+            const newState = { ...prev, [ingredientId]: selection };
+            console.log("Updated selections:", newState);  // Debugging line
+            return newState;
+        });
+    };
+
+    const handleServingChange = (newServings) => {
+        setServings(newServings);
+    };
+
+    const calculateTotalPrice = () => {
+        let basePrice = parseFloat(price);
+    
+        let additionalPrice = 0;
+        Object.values(ingredientSelections).forEach(selection => {
+            if (selection === 'double' || selection === 'triple') {
+                additionalPrice += prices[selection];
+                console.log(selection)
+                console.log(`Added ${prices[selection]} for ${selection}`);  // Debugging line
+            }
+        });
+    
+        let totalPrice = (basePrice + additionalPrice) * servings;
+        console.log("Total Price Calculation:", basePrice, "+", additionalPrice, "*", servings, "=", totalPrice);  // Debugging line
+        return totalPrice;
+    };
+
     return (
         <div>
-        
-        
-        <div className="nav_Header" style={customBackgroundStyle}>
-            <NavigationHeader/>
+            <div className="nav_Header" style={customBackgroundStyle}>
+                <NavigationHeader />
 
-            <div className="content-container">
-                <div className="image-container">
-                    <img src={imageSrc} alt="Item" className="item-image"/>
-                    <div className="item-name">{itemName}</div>
-                    <div className="item-price">{price}</div>
+                <div className="content-container">
+                    <div className="image-container">
+                        <img src={imageSrc} alt="Item" className="item-image" />
+                        <div className="item-name">{itemName}</div>
+                        <div className="item-price">Base Price: ${price}</div>
+                    </div>
+
+                    <div className="description-container">
+                        <p>{longDescri}</p>
+                    </div>
                 </div>
 
-                <div className="description-container">
-                    <p>{longDescri}</p>
+                <div className="ingredients-box">
+                    {editMode &&
+                        <ul>
+                            <li>
+                                <div className="ingredient-name">Noodles</div>
+                                <div className="action-buttons">
+                                <IngredientButtonGroup 
+                                    groupId="ingredient1" 
+                                    onSelect={handleSelectionChange}
+                                />
+                                </div>
+                                <div className="ingredient-name">Protein</div>
+                                <div className="action-buttons">
+                                <IngredientButtonGroup 
+                                    groupId="ingredient2" 
+                                    onSelect={handleSelectionChange}
+                                />
+                                </div>
+                                <div className="ingredient-name">Mushroom</div>
+                                <div className="action-buttons">
+                                <IngredientButtonGroup 
+                                    groupId="ingredient3" 
+                                    onSelect={handleSelectionChange}
+                                />
+                                </div>
+                                <div className="ingredient-name">Egg</div>
+                                <div className="action-buttons">
+                                <IngredientButtonGroup 
+                                    groupId="ingredient4" 
+                                    onSelect={handleSelectionChange}
+                                />
+                                </div>
+                                <div className="ingredient-name">Green Scallion</div>
+                                <div className="action-buttons">
+                                <IngredientButtonGroup 
+                                    groupId="ingredient5" 
+                                    onSelect={handleSelectionChange}
+                                />
+                                </div>
+                            </li>
+                        </ul>
+                    }
+                    <div className="add-to-cart-section">
+                        <ServingCounter onServingChange={handleServingChange} />
+                        <button className="add-to-cart-btn">Add to Cart</button>
+                        <div className="total-price">
+                            Total Price: ${calculateTotalPrice().toFixed(2)}
+                        </div>
+                    </div>
                 </div>
             </div>
-
-            <div className="ingredients-box">
-                {editMode&&
-                <ul>
-                
-                <li>
-                    <div className="ingredient-name">Noodles</div>
-                    <div className="action-buttons">
-                        <IngredientButtonGroup groupId="ingredient1" />
-                    </div>
-                </li>
-
-                <li>
-                    <div className="ingredient-name">Broth</div>
-                    <div className="action-buttons">
-                        <IngredientButtonGroup groupId="ingredient2" />
-                    </div>
-                </li>
-
-
-                <li>
-                    <div className="ingredient-name">Pork</div>
-                    <div className="action-buttons">
-                        <IngredientButtonGroup groupId="ingredient3" />
-                    </div>
-                </li>
-
-
-                <li>
-                    <div className="ingredient-name">Green Scallion</div>
-                    <div className="action-buttons">
-                        <IngredientButtonGroup groupId="ingredient4" />
-                    </div>
-                </li>
-            </ul>
-            }
-           <div className="add-to-cart-section">
-                <ServingCounter />
-                <button className="add-to-cart-btn">Add to Cart</button>
-           </div>
-        </div>
-        </div>
         </div>
     );
 }
