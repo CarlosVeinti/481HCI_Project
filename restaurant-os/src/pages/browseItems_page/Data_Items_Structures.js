4e6b040aaedd68106c8d96b988af804147ae61f8
CarlosVeinti <carlosveintimillam@hotmail.com>
2023-12-01 22:33:13 -0700

    Every Category has 3 items now (minimum)

--- restaurant-os/src/pages/browseItems_page/Data_Items_Structures.js
+++ restaurant-os/src/pages/browseItems_page/Data_Items_Structures.js
@@ -54,6 +54,20 @@ const itemData = {
         imageSrc: "/Sushi/californiaRoll.jpg",
         price: 8.99,
         longDescri: "Avocado, Crab and Cucumber",
+      },
+      {
+        itemName: "Philadelphia Roll",
+        briefDescri: "8 pieces",
+        imageSrc: "/Sushi/philadelphiaroll.jpg",
+        price: 17.99,
+        longDescri: "Cream Cheese, Salmon, Avocado, Cucumber, Sesame Seeds"
+      },
+      {
+        itemName: "Dynamite Roll",
+        briefDescri: "8 pieces",
+        imageSrc: "/Sushi/dynamiteroll.jpg",
+        price: 16.99,
+        longDescri: "Shrimp tempura, avocado, cucumber",
       }
         
     ],
@@ -64,8 +78,22 @@ const itemData = {
         itemName:"Salmon Sashimi",
         briefDescri:"6 pieces",
         imageSrc: "/Sashimi/SalmonSashimi.webp",
-        price: 15.99,
+        price: 18.99,
         longDescri: "Fresh Salmon",
+      },
+      {
+        itemName:"Tuna Sashimi",
+        briefDescri:"4 pieces",
+        imageSrc: "/Sashimi/tunaSashimi.jpg",
+        price: 13.99,
+        longDescri: "Fresh Tuna",
+      },
+      {
+        itemName:"Surf Clam Sashimi",
+        briefDescri:"8 pieces",
+        imageSrc: "/Sashimi/surfClamSashimi.webp",
+        price: 23.99,
+        longDescri: "Fresh Tuna",
       }
 
     ],
@@ -78,8 +106,23 @@ const itemData = {
         briefDescri:"1 Bowl",
         imageSrc: "/Ramen/chickenRamen.jpg",
         price: 14.99,
-        longDescri: "Flavourful broth, roasted chicken, fresh veggies, noodles, soft cooked egg",
+        longDescri: "Flavourful broth, roasted chicken, fresh scallions, noodles, soft cooked egg",
+      },
+      {
+        itemName:"Hokkaido Ramen",
+        briefDescri:"1 Bowl",
+        imageSrc: "/Ramen/hokkaidoRamen.jpg",
+        price: 15.99,
+        longDescri: "Miso broth, noodles, pork, butter, corn, egg, scallions",
+      },
+      {
+        itemName:"Shoyu Ramen",
+        briefDescri:"1 Bowl",
+        imageSrc: "/Ramen/shoyuRamen.jpg",
+        price: 16.49,
+        longDescri: "Soy sauce based broth, noodles, bamboo shots, pork, scallions",
       }
+
     ],
 
 
@@ -87,11 +130,25 @@ const itemData = {
 
     desserts: [
       {
-        itemName:"Matcha Cake",
+        itemName:"Matcha Cheesecake",
         briefDescri:"Two Pieces",
         imageSrc: "/Desserts/matchaCake.png",
         price: 15.99,
         longDescri: "Light and fluffly with the traditional matcha flavour",
+      },
+      {
+        itemName:"Mochi Ice Cream",
+        briefDescri:"5 Pieces",
+        imageSrc: "/Desserts/mochiIceCream.webp",
+        price: 19.99,
+        longDescri: "Soft chewy out layer of glutinous rice, filled with ice cream",
+      },
+      {
+        itemName:"Dorayaki",
+        briefDescri:"3 Pieces",
+        imageSrc: "/Desserts/Dorayaki.jpg",
+        price: 18.00,
+        longDescri: " Sweet adzuki bean paste sandwiched between soft honey pancakes",
       }
     ],
 
@@ -102,14 +159,14 @@ const itemData = {
       {
         itemName:"Sapporo Beer",
         briefDescri:"1 Bottle",
-        imageSrc: "/Beverages/sapporoBeerBottle.jpeg",
+        imageSrc: "/Beverages/sapporoBeerBottle.webp",
         price: 3.49,
         longDescri: "Taste of Malt, cereal, herbs, hops, honey, apple",
       },
       {
         itemName:"Coca-Cola",
         briefDescri:"1 Bottle",
-        imageSrc: "/Beverages/cocaCola.jpeg",
+        imageSrc: "/Beverages/cocaColaBottle.jpg",
         price: 2.99,
         longDescri: "Original Coca-Cola",
       },
@@ -117,7 +174,7 @@ const itemData = {
         itemName:"Green Tea",
         briefDescri:"1 Tea Pot",
         imageSrc: "/Beverages/GreenTeaPot.webp",
-        price: 2.99,
+        price: 11.99,
         longDescri: "64 oz serving",
       }
 
@@ -130,7 +187,21 @@ const itemData = {
         imageSrc: "/Tempura/ShrimpTempura.png",
         price: 12.99,
         longDescri: "Battered and deep fried",
-      }
+      },
+      {
+        itemName: "Eggplant Tempura",
+        briefDescri: "8 pieces",
+        imageSrc: "/Tempura/eggplant_Tempura.webp",
+        price: 16.00,
+        longDescri: "Battered and deep fried",
+      },
+      {
+        itemName: "Sweet Potato Tempura",
+        briefDescri: "6 pieces",
+        imageSrc: "/Tempura/sweetPotato_Tempura.jpg",
+        price: 10.00,
+        longDescri: "Battered and deep fried",
+      },
     ],
 
 
