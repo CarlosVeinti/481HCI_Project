5656f4c5cc863f1325d94a59a3400ebab8901ba1
CarlosVeinti <carlosveintimillam@hotmail.com>
2023-12-03 14:23:34 -0700

    Correct Servings in Cart Page

--- restaurant-os/src/components/CartItem/CartItem.js
+++ restaurant-os/src/components/CartItem/CartItem.js
@@ -3,24 +3,26 @@ import { FaTrash } from 'react-icons/fa';
 import './CartItem.css';
 
 function CartItem({ item, removeFromCart, increaseQuantity, decreaseQuantity }) {
-  const { itemName, description, quantity, price, imageSrc } = item;
+  const { itemName, description, quantity, price, imageSrc, servings } = item;
   console.log("Item:", item);
+
   return (
     <div className="cart-item">
       <div className="item-details">
-        <img src={item.imageSrc} alt="Food Item" className="food-image" />
+        <img src={imageSrc} alt="Food Item" className="food-image" />
         <div className="item-info">
-          <h3>{item.itemName}</h3>
-          <p>{item.description}</p>
+          <h3>{itemName}</h3>
+          <p>{description}</p>
+          
         </div>
       </div>
       <div className="quantity-container">
         <button className="btn btn-outline-secondary" onClick={() => decreaseQuantity(itemName)}>-</button>
-        <span>{item.quantity}</span>
+        <span>{servings}</span>
         <button className="btn btn-outline-secondary" onClick={() => increaseQuantity(itemName)}>+</button>
       </div>
-      <span className="cart-item-price">${(item.totalPrice ).toFixed(2)}</span>
-      <button className="btn btn-link remove-button" onClick={() => removeFromCart(item.itemName)}>
+      <span className="cart-item-price">${(item.totalPrice).toFixed(2)}</span>
+      <button className="btn btn-link remove-button" onClick={() => removeFromCart(itemName)}>
         <FaTrash />
       </button>
     </div>
